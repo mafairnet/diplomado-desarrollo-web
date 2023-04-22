a467ce85bd3be50d1e7c6212ef4df965ca88b8d2
Miguel Torres <mafairnet@gmail.com>
2023-04-22 11:49:24 -0500

    Se agrega proyecto del API
    Se agrega Proyecto del API

---
+++ herramienrtas-adicionales-asp-net/ControlEscolaApi/ControlEscolaApi/Program.cs
@@ -0,0 +1,42 @@
+using ControlEscolaApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlEscolaApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            //Agregando conectividad con BD
+            builder.Services.AddDbContext<PaisContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("PaisContext")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
